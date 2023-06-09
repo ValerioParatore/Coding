1dad50e28ca445183663238493c36415e26c4757
ValerioParatore <valerioteoparatore@gmail.com>
2023-06-09 16:52:59 +0200

    BENCHMARK

--- Epicode/U1.W3.D4/script.js
+++ Epicode/U1.W3.D4/script.js
@@ -1,29 +1,17 @@
 
 let tabellaDiv = document.getElementById('tabella')
 let numeroRandom = document.getElementById('numero-estratto')
+let allNumbers = []
 let numbersEx = []
+let playersN = []
 let tabellinaDiv = document.getElementById('tabellina')
+let randomNumber = Math.floor(Math.random()*76) + 1
 
 
 
 
 
 
-
-
-const getRandomNumber = function(){
-    let randomNumber = Math.floor(Math.random()*76) + 1
-    return randomNumber
-}
-
-
-
-
-const renderGame = function(){
-    let numeroEstratto = getRandomNumber()
-    numbersEx.push(numeroEstratto)
-    numeroRandom.innerText = numeroEstratto
-}
 
 
 
@@ -36,11 +24,16 @@ const tabellaMaker = function(numberCell){
         cellTabella.classList.add('cell')
         let cellValue = document.createElement('h3')
         cellValue.innerText = i + 1
+        let nofCell = parseInt(cellValue.inn)
+        allNumbers.push(i)
+        numbersEx.forEach(n =>{
+            if(n === cellValue.innerText){
+                cellTabella.classList('cell.selected ')
+            }
+        })
+
         cellTabella.appendChild(cellValue)
         tabellaDiv.appendChild(cellTabella)
-      //  if(numeroEstratto[i] === i){
-         //   cellTabella.classList('color-epic')
-     //   }
     }
 
 }
@@ -49,15 +42,26 @@ const tabellaMaker = function(numberCell){
 
 const tabellinaMaker = function(numberCell){
     for(let i = 0; i<numberCell; i++){
+        let rnd = Math.floor(Math.random()*numberCell) + 1
+        if(!playersN.includes(rnd)){
+            playersN.push(rnd);
+        }else {
+            i--;
+        
+        }
+        playersN.sort(function (a, b) {
+            return a - b;
+          });
         let cellTabellina = document.createElement('div')
         cellTabellina.classList.add('cell-tabellina')
         let cellValue = document.createElement('h3')
         let space = document.createElement('hr')
-        cellValue.innerText = i + 1
-        cellTabellina.appendChild(cellValue).appendChild(space)
-        tabellinaDiv.appendChild(cellTabellina)
+        cellValue.innerText = playersN[rnd]
+        cellTabellina.appendChild(cellValue)
+        cellTabellina.appendChild(tabellinaDiv)
         
     }
+    
 }
 
 
@@ -68,3 +72,7 @@ const tabellinaMaker = function(numberCell){
 
 
 tabellaMaker(76)
+
+
+
+//https://github.com/AlessioDiPretoro/14_23.06.08
