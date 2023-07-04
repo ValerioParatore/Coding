ebd7791256026fa16d8b6e377bc82537adf88775
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-07-04 17:14:28 +0200

    Local storage

--- Epicode/U2.W3.D2/esercizio2Script.js
+++ Epicode/U2.W3.D2/esercizio2Script.js
@@ -2,4 +2,3 @@ const spanCounter = document.getElementById('counter')
 let counterEl = sessionStorage.getItem('counter-memory')
 
 setInterval(()=>{counterEl++,sessionStorage.setItem('counter-memory', counterEl),spanCounter.textContent = sessionStorage.getItem('counter-memory')}, 1000)
-
